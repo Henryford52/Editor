491ba0b07c6e2d2728f51f3986ec765d744c87fb
Henryford52 <henryuzzi75@gmail.com>
2024-05-14 19:07:32 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import CustomEditor from './components/Editor'
 import EditorComponent from './components/Editor'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import Navbar from './components/navbar'
+import Home from './components/home';
+import Section from './components/section';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/Section",
+      element: <Section />
+    },
+   
+    
+
+  ]);
 
   return (
     <>
     {/* <CustomEditor/> */}
-    <Navbar/>
-    <EditorComponent/>
+    {/* <Navbar/>
+    <EditorComponent/> */}
+     <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/components/section/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div>
+      <h1 style={{color: "white"}}>Welcome To My website</h1>  
+    </div>
+  )
+}
+
+export default Section
